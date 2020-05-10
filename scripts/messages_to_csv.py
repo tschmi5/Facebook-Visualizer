663ceb48c53175dac5c9f31fef6e6a4a792c43e6
Tyler Schmitz <tschmitz@clearedgeit.com>
2020-05-09 23:39:38 -0400

    Message Timeline now implemented

--- scripts/messages_to_csv.py
+++ scripts/messages_to_csv.py
@@ -4,21 +4,28 @@ import csv
 
 messages_location = '../data/facebook/messages/inbox'
 
-for root,dirs,files in os.walk(messages_location):
-  for name in dirs:
-    person_path = os.path.join(root,name,'message_1.json')
-    f = open(person_path)
-    raw_json = json.load(f)
-    places = (raw_json['off_facebook_activity'])
-    f.close()
+with open(messages_location + '/../messages_flat.csv','w') as file:
+  writer = csv.writer(file)
+  writer.writerow(['title','sender_name','timestamp_ms','content','type'])
+  for root,dirs,files in os.walk(messages_location):
+    for name in dirs:
+      person_path = os.path.join(root,name,'message_1.json')
+      try:
+        f = open(person_path)
+        raw_json = json.load(f)
+      
+        messages = (raw_json['messages'])
+        title = raw_json['title']
+        for message in messages:
+          try:
+            writer.writerow([title,message['sender_name'],message['timestamp_ms'],message['content'],message['type']])
+          except KeyError:
+            pass
+        f.close()
+      except FileNotFoundError:
+        f.close()
+        pass
 
 
 
 
-
-# with open(off_facebook_file + '.csv','w') as file:
-#   writer = csv.writer(file)
-#   writer.writerow(['name','id','type','date'])
-#   for place in places:
-#     for event in place['events']:
-#       writer.writerow([place['name'],event['id'],event['type'],event['timestamp']])
